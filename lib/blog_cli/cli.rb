53e84bc266a7d7bd67b71d8d1bcb931150b103f3
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-16 17:15:40 +0000

    revised cli.rb to include required methods for BlogCLI::CLI class

--- lib/blog_cli/cli.rb
+++ lib/blog_cli/cli.rb
@@ -1,4 +1,46 @@
 class BlogCLI::CLI
   attr_reader :last_input, :current_user 
   
+  def call
+    puts "Welcome to your CLI Blog!"
+    login
+    menu
+  end
+  
+  def login 
+    puts "Please enter your name to login:"
+    @current_user = BlogCLI::Author.find_or_create_by(:name => user_input)
+    puts "You're now logged in as: #{current_user.name}"
+  end
+  
+  def menu
+    puts "What would you like to do?"
+    puts "1. Write a post"
+    puts "2. List your posts"
+    puts "3. List all posts"
+    main_menu_loop
+  end
+  
+  def main_menu_loop 
+    
+  end 
+  
+  def post_index
+    
+  end
+  
+  def post_show
+    
+  end
+  
+  def post_new
+    
+  end
+  
+  private
+  
+  def user_input
+    @last_input = gets.strip
+  end
+  
 end 
