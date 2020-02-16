8e7586490497b61127c66a9b80b96aa46f440f33
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-16 16:13:28 +0000

    created migrate dir. & revised library dir.

--- lib/blog_cli.rb
+++ lib/blog_cli.rb
@@ -1,6 +1,15 @@
-require "blog_cli/version"
+require_relative '../config/environment'
+
+ActiveRecord::Migration.check_pending!
 
 module BlogCli
   class Error < StandardError; end
   # Your code goes here...
 end
+
+
+require "blog_cli/version"
+require "blog_cli/cli"
+require "blog_cli/post"
+require "blog_cli/author"
+require "blog_cli/category"
