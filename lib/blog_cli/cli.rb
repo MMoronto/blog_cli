78af2f1f0ecc9c0273f9d254bce95c771c548e36
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-16 17:48:22 +0000

    revised cli.rb file

--- lib/blog_cli/cli.rb
+++ lib/blog_cli/cli.rb
@@ -22,19 +22,68 @@ class BlogCLI::CLI
   end
   
   def main_menu_loop 
-    
+    while user_input != "exit"
+      case last_input.to_i
+      when 1
+        post_new
+        break
+      when 2
+        post_index
+        break
+      else
+        menu
+        break
+      end
+    end
   end 
   
   def post_index
+    puts "---Posts by you, #{current_user.name} ---"
+    #How to print out each post by this user with an ID and a Title
+    current_user.posts.each do |post|
+      puts "#{post.id}. #{post.title}"
+    end 
     
+    puts "Enter the post ID you'd like to see or edit or go back to the main menu:"
+    if user_input.to_i > 0
+      post_show
+    else
+      menu
+    end
   end
   
   def post_show
+    puts "Loading Post #{last_input}..."
+    #When we load this post, it 100% belongs to the current user_input
     
+    #begin
+    # post = current_user.posts.find(last_input)
+    if post = current_user.posts.find_by(:id => last_input)
+      puts "---#{post.id} ---#{post.title}"
+      puts
+      puts post.content
+    else
+    # rescue ActiveRecord::RecordNotFound
+      puts "Can't find a post with ID #{last_input} for you..."
+    end
+    menu 
   end
   
   def post_new
+    params = {}
     
+    puts "Please enter the title of your new post:"
+    params[:title] = user_input
+    
+    puts "Please enter the content of your new post:"
+    params[:content] = user_input
+    
+    # 2. Let's actually instantiate the post already associated with the 
+    # current_user
+    post = current_user.posts.build(params)
+    post.save # Insert the Post with the author_id FK
+    puts "Saved Post ##{post.id}..."
+    menu
   end
   
   private
